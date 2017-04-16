a51992d2d96d2de2ae9d259f3083a6a74567b9ea
dszshen <1812890566@qq.com>
2017-04-16 23:52:35 +0800

    000

--- src/main/resources/assets/js/controllers/role/list.js
+++ src/main/resources/assets/js/controllers/role/list.js
@@ -16,20 +16,11 @@ XXAPP.controller('RoleListController',function($rootScope,$scope,$http,$uibModal
     });
 
     $scope.addPermission = function () {
-        $scope.treeModal ={
-            header:"为角色授权",
-            title:"权限树列表"
-        };
-
         $uibModal.open({
-            //templateUrl: CONTEXT+'/pages/system/role/modal/role_pms.html?version=' + version,
-            templateUrl:CONTEXT+'/common/modal/tree_modal.html',
-            controller: 'TreeModalCtrl',
-            resolve: {
-                treeModal: function () {
-                    return $scope.treeModal;
-                }
-            }
+            templateUrl:CONTEXT+'/pages/role/add_permission_modal.html',
+            controller: 'RolePermissionAddCtrl',
+            size: 'lg',
+            backdrop: 'static'
         });
     };
 
