aa8dced6e76bae164f281dd0f72a739c6a9deaae
dszshen <1812890566@qq.com>
2017-04-17 17:49:14 +0800

    update the css of login page

--- src/main/java/com/bs3/manage/controller/system/SystemController.java
+++ src/main/java/com/bs3/manage/controller/system/SystemController.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.bs3.manage.common.util.Constant;
 import com.bs3.manage.common.util.JsonResult;
 import com.bs3.manage.service.system.SystemService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -12,28 +13,77 @@ import javax.annotation.Resource;
 /**
  * Created by zhangbin on 2016/6/26.
  * 系统管理
- *
  */
 @RestController
 @RequestMapping("sys")
 public class SystemController {
+
     @Resource
     private SystemService systemService;
 
-    @RequestMapping(method= RequestMethod.GET,value="base")
-    public JsonResult getBase(@RequestParam("id") String id){
+    /**
+     * 查询配置
+     *
+     * @param type 请求路径
+     * @param id   查询参数
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "{type:base|security|email}")
+    public JsonResult configGet(@PathVariable String type, @RequestParam("id") String id) {
         JSONObject params = new JSONObject();
-        params.put("id",id);
-        return systemService.config(Constant.SYSTEM_CONFIG_BASE,params,Constant.DATABASE_OP_SELECT);
+        params.put("id", id);
+        return request("get", type, params);
     }
 
-    @RequestMapping(method= RequestMethod.POST,value="base")
-    public JsonResult baseAdd(@RequestBody JSONObject params){
-        return systemService.config(Constant.SYSTEM_CONFIG_BASE,params,Constant.DATABASE_OP_INSERT);
+    /**
+     * 添加配置信息
+     *
+     * @param type
+     * @param params
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "{type:base|security|email}")
+    public JsonResult configAdd(@PathVariable String type, @RequestBody JSONObject params) {
+        return request("post", type, params);
     }
 
-    @RequestMapping(method= RequestMethod.PUT,value="base")
-    public JsonResult baseUpdate(@RequestBody JSONObject params){
-        return systemService.config(Constant.SYSTEM_CONFIG_BASE,params,Constant.DATABASE_OP_UPDATE);
+    /**
+     * 配置更新
+     *
+     * @param type
+     * @param params
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.PUT, value = "{type:base|security|email}")
+    public JsonResult configUpdate(@PathVariable String type, @RequestBody JSONObject params) {
+        return request("put", type, params);
+    }
+
+    /**
+     * @param reqType 请求方式
+     * @param reqPath 请求路径
+     * @param params  请求参数
+     * @return 返回对应的数据
+     */
+    private JsonResult request(String reqType, String reqPath, JSONObject params) {
+
+        String opType = Constant.DATABASE_OP_SELECT;
+        if (reqType.equals("post")) {
+            opType = Constant.DATABASE_OP_INSERT;
+        } else if (reqType.equals("put")) {
+            opType = Constant.DATABASE_OP_UPDATE;
+        }
+
+        if (StringUtils.isBlank(reqPath)) {
+            return JsonResult.failure("对不起，请求路径错误！");
+        } else if (reqPath.equals("base")) {
+            return systemService.config(Constant.SYSTEM_CONFIG_BASE, params, opType);
+        } else if (reqPath.equals("security")) {
+            return systemService.config(Constant.SYSTEM_CONFIG_SECURITY, params, opType);
+        } else if (reqPath.equals("email")) {
+            return systemService.config(Constant.SYSTEM_CONFIG_EMAIL, params, opType);
+        }
+
+        return JsonResult.failure("对不起，请求路径错误！");
     }
 }
--- src/main/java/com/bs3/manage/service/system/SystemService.java
+++ src/main/java/com/bs3/manage/service/system/SystemService.java
@@ -12,31 +12,31 @@ import java.util.UUID;
 
 /**
  * Created by zhangbin on 2016/6/26.
+ * 系统管理service
  */
 @Service
-public class SystemService extends BaseService{
+public class SystemService extends BaseService {
 
     private static final Logger LOGGER = Logger.getLogger(SystemService.class);
 
     /**
-     *
-     * @param type 设置类型
+     * @param type   设置类型
      * @param params 设置参数
      * @param opType 操作数据库类型，默认为update
      */
-    public JsonResult config(String type, JSONObject params, String opType){
-        if(StringUtils.isBlank(opType)){
+    public JsonResult config(String type, JSONObject params, String opType) {
+        if (StringUtils.isBlank(opType)) {
             opType = Constant.DATABASE_OP_UPDATE;
         }
         //sql语句
         String sql = "";
 
-        if(StringUtils.isNotBlank(type)){
-            if(type.equals(Constant.SYSTEM_CONFIG_BASE)){
-                if(opType.equals(Constant.DATABASE_OP_SELECT)){
-                    sql="SELECT * FROM config_company WHERE id=?";
-                    return JsonResult.success(find(sql,params.get("id")));
-                }else if(opType.equals(Constant.DATABASE_OP_INSERT)){
+        if (StringUtils.isNotBlank(type)) {
+            if (type.equals(Constant.SYSTEM_CONFIG_BASE)) {
+                if (opType.equals(Constant.DATABASE_OP_SELECT)) {
+                    sql = "SELECT * FROM config_company WHERE id=?";
+                    return JsonResult.success(find(sql, params.get("id")));
+                } else if (opType.equals(Constant.DATABASE_OP_INSERT)) {
                     sql = "INSERT INTO config_company(id,name,addr,employees,main_business,tel,mobile_number,legal_person,create_date,description) values (?,?,?,?,?,?,?,?,?,?)";
                     String[] baseParams = new String[]{
                             UUID.randomUUID().toString(),
@@ -50,9 +50,9 @@ public class SystemService extends BaseService{
                             params.getString("create_date"),
                             params.getString("description")
                     };
-                    return JsonResult.success(insert(sql,baseParams));
-                }else if(opType.equals(Constant.DATABASE_OP_UPDATE)){
-                    sql = "UPDATE config_company SET name=?,addr=?,employees=?,main_business=?,tel=?,mobile_number=?,legal_person=?,create_date=?,description=? WHERE id='"+params.getString("id")+"'";
+                    return JsonResult.success(insert(sql, baseParams));
+                } else if (opType.equals(Constant.DATABASE_OP_UPDATE)) {
+                    sql = "UPDATE config_company SET name=?,addr=?,employees=?,main_business=?,tel=?,mobile_number=?,legal_person=?,create_date=?,description=? WHERE id='" + params.getString("id") + "'";
                     String[] baseParams = new String[]{
                             params.getString("name"),
                             params.getString("addr"),
@@ -64,13 +64,13 @@ public class SystemService extends BaseService{
                             params.getString("create_date"),
                             params.getString("description")
                     };
-                    return JsonResult.success(update(sql,baseParams));
+                    return JsonResult.success(update(sql, baseParams));
                 }
-            }else if(type.equals(Constant.SYSTEM_CONFIG_SECURITY)){
-                if(opType.equals(Constant.DATABASE_OP_SELECT)){
-                    sql="SELECT * FROM config_security WHERE id=?";
-                    return JsonResult.success(find(sql,params.get("id")));
-                }else if(opType.equals(Constant.DATABASE_OP_INSERT)){
+            } else if (type.equals(Constant.SYSTEM_CONFIG_SECURITY)) {
+                if (opType.equals(Constant.DATABASE_OP_SELECT)) {
+                    sql = "SELECT * FROM config_security WHERE id=?";
+                    return JsonResult.success(find(sql, params.get("id")));
+                } else if (opType.equals(Constant.DATABASE_OP_INSERT)) {
                     sql = "INSERT INTO config_security(id,white_list,forbid_list,max_conn) values (?,?,?,?)";
                     String[] baseParams = new String[]{
                             UUID.randomUUID().toString(),
@@ -78,21 +78,21 @@ public class SystemService extends BaseService{
                             params.getString("forbid_list"),
                             params.getString("max_conn")
                     };
-                    return JsonResult.success(insert(sql,baseParams));
-                }else if(opType.equals(Constant.DATABASE_OP_UPDATE)){
-                    sql = "UPDATE config_security SET white_list=?,forbid_list=?,max_conn=? WHERE id='"+params.getString("id")+"'";
+                    return JsonResult.success(insert(sql, baseParams));
+                } else if (opType.equals(Constant.DATABASE_OP_UPDATE)) {
+                    sql = "UPDATE config_security SET white_list=?,forbid_list=?,max_conn=? WHERE id='" + params.getString("id") + "'";
                     String[] baseParams = new String[]{
                             params.getString("white_list"),
                             params.getString("forbid_list"),
                             params.getString("max_conn")
                     };
-                    return JsonResult.success(update(sql,baseParams));
+                    return JsonResult.success(update(sql, baseParams));
                 }
-            }else if(type.equals(Constant.SYSTEM_CONFIG_SERVER)){
+            } else if (type.equals(Constant.SYSTEM_CONFIG_SERVER)) {
 
             }
         }
-        return JsonResult.failure("对不起，操作失败！",null);
+        return JsonResult.failure("对不起，操作失败！", null);
     }
 
 
