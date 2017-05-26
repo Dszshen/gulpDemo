331eab68f78db9168f1b3872b950a571c23f85f2
dszshen <1812890566@qq.com>
2017-05-26 17:57:18 +0800

    weixin 开发更新

--- src/main/java/com/bs3/manage/controller/weixin/WeixinApi.java
+++ src/main/java/com/bs3/manage/controller/weixin/WeixinApi.java
@@ -1,6 +1,12 @@
 package com.bs3.manage.controller.weixin;
 
+import com.belerweb.social.weixin.bean.EventType;
+import com.belerweb.social.weixin.bean.Message;
+import com.belerweb.social.weixin.bean.MsgType;
 import com.bs3.manage.service.weixin.WeixinService;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by Administrator on 2017/5/25 0025.
@@ -18,23 +26,53 @@ public class WeixinApi {
     private static final String TOKEN = "";
     @Resource
     private WeixinService weixinService;
+
     /**
-     * 验证微信请求是否成功
+     * 验证微信来源的有效性
+     * @param signature
+     * @param timestamp
+     * @param nonce
+     * @param echostr
      * @param request
+     * @return
      */
     @RequestMapping(method = RequestMethod.GET, value = "/api/weixin")
-    public void weixinApi(@RequestParam String signature,
+    public ResponseEntity<byte[]> weixinApi(@RequestParam String signature,
                           @RequestParam String timestamp,
                           @RequestParam String nonce,
                           @RequestParam String echostr, HttpServletRequest request){
-        String [] checkStr= new String[]{TOKEN,timestamp,nonce};
-        String str1 = "";
-        for (String str:checkStr) {
-            str1+=str;
+        if (!weixinService.validate(signature, timestamp, nonce)) {// 验证消息真实性
+            return new ResponseEntity<byte[]>(HttpStatus.FORBIDDEN);
+        }
+        return new ResponseEntity<byte[]>(echostr.getBytes(), HttpStatus.OK);
+    }
+
+    /**
+     * 微信post接口
+     * @param signature
+     * @param timestamp
+     * @param nonce
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/api/weixin")
+    public ResponseEntity<byte[]> api(@RequestParam String signature,
+                                      @RequestParam String timestamp, @RequestParam String nonce, HttpServletRequest request)
+            throws IOException {
+        if (!weixinService.validate(signature, timestamp, nonce)) {
+            return new ResponseEntity<byte[]>(HttpStatus.FORBIDDEN);
         }
 
-        //进行sha1加密
-
-
+        Message message = Message.parse(IOUtils.toString(request.getInputStream()));
+        weixinService.saveMessage(message);// 保存消息到数据库
+        if (message.getEventType() == EventType.CLICK && message.getEventKey().equals("CS")) {
+            Message response = new Message(MsgType.TRANSFER_CUSTOMER_SERVICE);
+            response.setToUser(message.getFromUser());
+            response.setFromUser(message.getToUser());
+            response.setCreateTime(new Date());
+            return new ResponseEntity<byte[]>(response.toXML().getBytes(), HttpStatus.OK);
+        }
+        return new ResponseEntity<byte[]>(HttpStatus.OK);
     }
 }
--- src/main/java/com/bs3/manage/service/weixin/WeixinService.java
+++ src/main/java/com/bs3/manage/service/weixin/WeixinService.java
@@ -1,11 +1,38 @@
 package com.bs3.manage.service.weixin;
 
+import com.belerweb.social.weixin.api.Weixin;
+import com.belerweb.social.weixin.bean.Message;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Administrator on 2017/5/25 0025.
  */
 @Service
 public class WeixinService {
+
+    @Resource
+    private Weixin weixin;
+
+    /**
+     * 验证微信来源的有效性
+     * @param signature
+     * @param timestamp
+     * @param nonce
+     * @return
+     */
+    public boolean validate(String signature, String timestamp, String nonce) {
+        return weixin.validate(signature, timestamp, nonce);
+    }
+
+    public boolean saveMessage(Message message) {
+        //boolean success = weixinMpMessageDao.insert(message) == 1;
+        synchronized (this) {
+            notify();
+        }
+        //LOGGER.debug("notify mp message response process.");
+        return true;
+    }
 
 }
